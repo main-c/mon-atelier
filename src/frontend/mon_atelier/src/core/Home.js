695d500da5862fdf75944cbe57979da843edac9c
Rainsong <ngoutsoprainsong@gmail.com>
2021-12-30 18:53:56 +0100

    home ready, waiting for api and suggestions

--- src/frontend/mon_atelier/src/core/Home.js
+++ src/frontend/mon_atelier/src/core/Home.js
@@ -194,7 +194,153 @@ function Home(){
 						</div>
 					</div>
 				</div>
-					
+
+				<div>
+					<div className='fw-bold text-center mt-5 fs-2'>A PROPOS</div>
+					<div className="mx-auto" style={{height:'6px', width:'100px', background:'#FF5566'}}></div>
+
+					<div className="row container mx-auto d-flex">
+					<div className="col-lg-6">
+					<img src="images/APropos.png" className="card-img-top p-0" alt="..." style={{width:'500px', height:'50Opx'}}  />
+					</div>
+					<div className="col-lg-6">
+					<div><h3 className=" ps-5 fw-bold fs-2 pt-2" style={{color:'#443369', marginLeft:'320px'}}>MonAtelier,</h3></div>	
+					<div className="ms-auto" style={{height:'6px', width:'100px', background:'#443369'}}></div><br />
+					<p style={{textAlign:'right'}}>MonAtelier est une application web de couture dont l’objectif principal est de numériser 
+					l’organisation des activités des couturiers. En plus de créer une plate-forme qui permettra à chaque atelier 
+					ou couturiers de pouvoir partager ses modèles en ligne, nous permettrons la gestion des informations de 
+					leurs clients telle que leurs mesures, 
+					leurs commandes passées et les dates de livraison ainsi que l'amélioration de la visibilité de l’atelier.</p>
+					</div>	
+
+					</div>
+				</div>
+
+				<div>
+					<div className='fw-bold text-center mt-5 fs-2'>CONTACT</div>
+					<div className="mx-auto mb-3" style={{height:'6px', width:'100px', background:'#FF5566'}}></div>
+					<div className="container row pt-3 mx-auto">
+
+						<div className="card p-0 col-lg-1 mx-auto border-0" style={{width: "8rem"}}>
+						  <img src="images/banner1.jpg" className="card-img-top mx-auto p-0 rounded-pill" alt="..." style={{width:'60px', height:'60px'}}  />
+						  <div className="card-body ps-0">
+						    <h6 className="card-title text-center">Yannik Kadjie</h6>
+						  
+						    
+						    <div className="mx-auto text-center">
+						    	<a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto me-3" alt="..." style={{width:'30px', height:'3Opx'}}  /></a>
+						    <a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto" alt="..." style={{width:'30px', height:'3Opx'}}  /></a><br />
+						    </div>
+						    <div className="mx-auto mt-3 text-center">Frontend dev</div>
+						  </div>
+						</div>
+						<div className="card p-0 col-lg-1 mx-auto border-0" style={{width: "8rem"}}>
+						  <img src="images/banner1.jpg" className="card-img-top mx-auto p-0 rounded-pill" alt="..." style={{width:'60px', height:'60px'}}  />
+						  <div className="card-body ps-0">
+						    <h6 className="card-title text-center">Gaëlle Tamho</h6>
+						  
+						    
+						    <div className="mx-auto text-center">
+						    	<a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto me-3" alt="..." style={{width:'30px', height:'3Opx'}}  /></a>
+						    <a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto" alt="..." style={{width:'30px', height:'3Opx'}}  /></a><br />
+						    </div>
+						    <div className="mx-auto mt-3 text-center">Frontend dev</div>
+						  </div>
+						</div>
+						<div className="card p-0 col-lg-1 mx-auto border-0" style={{width: "8rem"}}>
+						  <img src="images/banner1.jpg" className="card-img-top mx-auto p-0 rounded-pill" alt="..." style={{width:'60px', height:'60px'}}  />
+						  <div className="card-body ps-0">
+						    <h6 className="card-title text-center">Raïssa Tiomela</h6>
+						  
+						    
+						    <div className="mx-auto text-center">
+						    	<a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto me-3" alt="..." style={{width:'30px', height:'3Opx'}}  /></a>
+						    <a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto" alt="..." style={{width:'30px', height:'3Opx'}}  /></a><br />
+						    </div>
+						    <div className="mx-auto mt-3 text-center">Frontend dev</div>
+						  </div>
+						</div>
+						<div className="card p-0 col-lg-1 mx-auto border-0" style={{width: "8rem"}}>
+						  <img src="images/banner1.jpg" className="card-img-top mx-auto p-0 rounded-pill" alt="..." style={{width:'60px', height:'60px'}}  />
+						  <div className="card-body ps-0">
+						    <h6 className="card-title text-center">Rainsong Ngoutsop</h6>
+						  
+						    
+						    <div className="mx-auto text-center">
+						    	<a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto me-3" alt="..." style={{width:'30px', height:'3Opx'}}  /></a>
+						    <a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto" alt="..." style={{width:'30px', height:'3Opx'}}  /></a><br />
+						    </div>
+						    <div className="mx-auto mt-3 text-center">Frontend dev</div>
+						  </div>
+						</div>
+						<div className="card p-0 col-lg-1 mx-auto border-0" style={{width: "8rem"}}>
+						  <img src="images/banner1.jpg" className="card-img-top mx-auto p-0 rounded-pill" alt="..." style={{width:'60px', height:'60px'}}  />
+						  <div className="card-body ps-0">
+						    <h6 className="card-title text-center">Ghislain Makolle</h6>
+						  
+						    
+						    <div className="mx-auto text-center">
+						    	<a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto me-3" alt="..." style={{width:'30px', height:'3Opx'}}  /></a>
+						    <a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto" alt="..." style={{width:'30px', height:'3Opx'}}  /></a><br />
+						    </div>
+						    <div className="mx-auto mt-3 text-center">Frontend dev</div>
+						  </div>
+						</div>
+					</div>
+
+				</div>
+
+				<div className="">
+					   <div className=" pt-3 mt-2 text-white" style={{backgroundColor:'#443369'}}>
+				        <div className="container-fluid">
+				            <div
+				                className="row d-flex align-items-center">
+				                <div className="col-lg-4 ps-5">
+				                    <div>
+				                       <a href="#"><img src="images/logo.png" className="card-img-top mx-auto p-0 me-3 rounded-pill" alt="..." style={{width:'100px', height:'100px'}} /> <span className="text-white fs-2 fw-bold">MonAtelier</span></a>
+				                    </div>
+				                    <div className="w-50">
+				                         MonAtelier est une application web de couture dont l’objectif 
+				                         principal est de numériser l’organisation des activités des couturiers
+				                    </div>
+				                </div>
+				                <div className="col-lg-4  text-center">
+				                	<span className="text-white fs-3 fw-bold">Liens rapides</span>
+				                    <div className="active">
+				                    	<a href="" style={{paddingTop: '60px'}}>Accueil</a><br className="mb-3 mt-2"/>
+				                    	<a href="" style={{paddingTop: '60px'}}>Catégories</a><br className="mb-3"/>
+				                    	<a href="" style={{paddingTop: '60px'}}>Modèles</a><br className="mb-3"/>
+				                    	<a href="" style={{paddingTop: '60px'}}>A Propos</a><br className="mb-3"/>
+				                    	<a href="" style={{paddingTop: '60px'}}>Contact</a>
+				                    </div>
+				                </div>
+				                <div className="col-lg-4  text-center">
+				                	<span className="text-white fs-3 fw-bold">Créer un atelier </span><br />
+				                    <a class=" font-weight-normal text-justify " href="#">
+				                    Avec MonAtelier, vous avez la possiblité de gérer votre atelier, entre autre: vos clients, 
+				                    vos commandes et même la possibilité d’améliorer votre visibilité. <br />
+									Enregistrez-vous.</a><br />
+									<div className='pt-2'>
+									<a href="#" style={{color: '#443369', backgroundColor:'white'}}
+				                     className=" font-weight-normal h6 d-inline-block p-2 rounded-pill mb-2 me-2">S'inscire</a>
+				                     <span>OU</span>
+				                     <a href="#" style={{color: 'white'}}
+				                     className=" font-weight-normal h6 d-inline-block p-2 border ms-2 border-2 border-white rounded-pill mb-2 me-2">Se connecter</a>
+
+									</div>
+				                    
+				                </div>
+
+            </div>
+        </div>
+			    </div><hr className="mb-0" style={{backgroundColor: '#c4c4c4'}} />
+			    <div className="d-flex align-items-center text-dark" style={{height: '7vh'}}>
+			        <p className="mx-auto"><span className="">Copyright © 2020 | </span>  
+			        <span>power | </span>
+			        <span className=" h6 my-0">Groupe 4 
+			        </span></p>
+			    </div>
+				</div>	
 			</div>
 
 	
