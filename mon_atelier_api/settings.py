e45a6741341d28b44f6ab7411fb1f64e1b7c3f46
main-c <yannikkwc@gmail.com>
2022-01-03 12:24:08 +0100

    correct staticfile storage

--- mon_atelier_api/settings.py
+++ mon_atelier_api/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" 
 
 
 # Dossier où seront stockés les médias
