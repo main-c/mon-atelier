b027793b58776ab51de0b0c6278c29febfafadd2
Rainsong <ngoutsoprainsong@gmail.com>
2022-01-03 11:53:55 +0100

    footer legacy managed

--- src/frontend/mon_atelier/src/components/Footer.js
+++ src/frontend/mon_atelier/src/components/Footer.js
@@ -6,7 +6,59 @@ function Footer(){
 
 	return(
 
-		<h1 className="text-secondary text-center mt-5"></h1>
+		
+				<div className="">
+					   <div className=" pt-3 mt-2 text-white" style={{backgroundColor:'#443369'}}>
+				        <div className="container-fluid">
+				            <div
+				                className="row d-flex align-items-center">
+				                <div className="col-lg-4 ps-5">
+				                    <div>
+				                       <a href="#"><img src="images/logo.png" className="card-img-top mx-auto p-0 me-3 rounded-pill" alt="..." style={{width:'100px', height:'100px'}} /> <span className="text-white fs-2 fw-bold">MonAtelier</span></a>
+				                    </div>
+				                    <div className="w-50">
+				                         MonAtelier est une application web de couture dont l’objectif 
+				                         principal est de numériser l’organisation des activités des couturiers
+				                    </div>
+				                </div>
+				                <div className="col-lg-4  text-center">
+				                	<span className="text-white fs-3 fw-bold">Liens rapides</span>
+				                    <div className="active">
+				                    	<a href="" style={{paddingTop: '60px'}}>Accueil</a><br className="mb-3 mt-2"/>
+				                    	<a href="" style={{paddingTop: '60px'}}>Catégories</a><br className="mb-3"/>
+				                    	<a href="" style={{paddingTop: '60px'}}>Modèles</a><br className="mb-3"/>
+				                    	<a href="" style={{paddingTop: '60px'}}>A Propos</a><br className="mb-3"/>
+				                    	<a href="" style={{paddingTop: '60px'}}>Contact</a>
+				                    </div>
+				                </div>
+				                <div className="col-lg-4  text-center">
+				                	<span className="text-white fs-3 fw-bold">Créer un atelier </span><br />
+				                    <a class=" font-weight-normal text-justify " href="#">
+				                    Avec MonAtelier, vous avez la possiblité de gérer votre atelier, entre autre: vos clients, 
+				                    vos commandes et même la possibilité d’améliorer votre visibilité. <br />
+									Enregistrez-vous.</a><br />
+									<div className='pt-2'>
+									<a href="#" style={{color: '#443369', backgroundColor:'white'}}
+				                     className=" font-weight-normal h6 d-inline-block p-2 rounded-pill mb-2 me-2">S'inscire</a>
+				                     <span>OU</span>
+				                     <a href="#" style={{color: 'white'}}
+				                     className=" font-weight-normal h6 d-inline-block p-2 border ms-2 border-2 border-white rounded-pill mb-2 me-2">Se connecter</a>
+
+									</div>
+				                    
+				                </div>
+
+            </div>
+        </div>
+			    </div><hr className="mb-0" style={{backgroundColor: '#c4c4c4'}} />
+			    <div className="d-flex align-items-center text-dark" style={{height: '7vh'}}>
+			        <p className="mx-auto"><span className="">Copyright © 2020 | </span>  
+			        <span>power | </span>
+			        <span className=" h6 my-0">Groupe 4 
+			        </span></p>
+			    </div>
+				</div>	
+
 
 	);
 }
--- src/frontend/mon_atelier/src/core/Home.js
+++ src/frontend/mon_atelier/src/core/Home.js
@@ -25,7 +25,7 @@ function Home(){
 				<div className="bg-light">
 
 					<div className='fw-bold text-center mt-5 fs-2'>CATEGORIES</div>
-					<div className="mx-auto" style={{height:'6px', width:'100px', background:'#FF5566'}}></div>
+					<div className="mx-auto" style={{height:'4px', width:'100px', background:'#FF5566'}}></div>
 					<div className="row container pt-3 mx-auto">
 
 						<div className="card col-lg-4 mb-3 p-0" style={{width: '340px', margin:'10px'}}>
@@ -109,7 +109,7 @@ function Home(){
 				</div>
 				<div>
 					<div className='fw-bold text-center mt-5 fs-2'>NOS ARTICLES</div>
-					<div className="mx-auto mb-5" style={{height:'6px', width:'100px', background:'#FF5566'}}></div>
+					<div className="mx-auto mb-5" style={{height:'4px', width:'100px', background:'#FF5566'}}></div>
 
 					<div className="container row pt-3 mx-auto">
 
@@ -197,7 +197,7 @@ function Home(){
 
 				<div>
 					<div className='fw-bold text-center mt-5 fs-2'>A PROPOS</div>
-					<div className="mx-auto" style={{height:'6px', width:'100px', background:'#FF5566'}}></div>
+					<div className="mx-auto" style={{height:'4px', width:'100px', background:'#FF5566'}}></div>
 
 					<div className="row container mx-auto d-flex">
 					<div className="col-lg-6">
@@ -218,7 +218,7 @@ function Home(){
 
 				<div>
 					<div className='fw-bold text-center mt-5 fs-2'>CONTACT</div>
-					<div className="mx-auto mb-3" style={{height:'6px', width:'100px', background:'#FF5566'}}></div>
+					<div className="mx-auto mb-3" style={{height:'4px', width:'100px', background:'#FF5566'}}></div>
 					<div className="container row pt-3 mx-auto">
 
 						<div className="card p-0 col-lg-1 mx-auto border-0" style={{width: "8rem"}}>
@@ -231,7 +231,7 @@ function Home(){
 						    	<a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto me-3" alt="..." style={{width:'30px', height:'3Opx'}}  /></a>
 						    <a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto" alt="..." style={{width:'30px', height:'3Opx'}}  /></a><br />
 						    </div>
-						    <div className="mx-auto mt-3 text-center">Frontend dev</div>
+						    <div className="mx-auto mt-3 text-center"><i>Backend dev</i></div>
 						  </div>
 						</div>
 						<div className="card p-0 col-lg-1 mx-auto border-0" style={{width: "8rem"}}>
@@ -244,7 +244,7 @@ function Home(){
 						    	<a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto me-3" alt="..." style={{width:'30px', height:'3Opx'}}  /></a>
 						    <a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto" alt="..." style={{width:'30px', height:'3Opx'}}  /></a><br />
 						    </div>
-						    <div className="mx-auto mt-3 text-center">Frontend dev</div>
+						    <div className="mx-auto mt-3 text-center"><i>Frontend dev</i></div>
 						  </div>
 						</div>
 						<div className="card p-0 col-lg-1 mx-auto border-0" style={{width: "8rem"}}>
@@ -257,7 +257,7 @@ function Home(){
 						    	<a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto me-3" alt="..." style={{width:'30px', height:'3Opx'}}  /></a>
 						    <a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto" alt="..." style={{width:'30px', height:'3Opx'}}  /></a><br />
 						    </div>
-						    <div className="mx-auto mt-3 text-center">Frontend dev</div>
+						    <div className="mx-auto mt-3 text-center"><i>Frontend dev</i></div>
 						  </div>
 						</div>
 						<div className="card p-0 col-lg-1 mx-auto border-0" style={{width: "8rem"}}>
@@ -270,7 +270,7 @@ function Home(){
 						    	<a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto me-3" alt="..." style={{width:'30px', height:'3Opx'}}  /></a>
 						    <a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto" alt="..." style={{width:'30px', height:'3Opx'}}  /></a><br />
 						    </div>
-						    <div className="mx-auto mt-3 text-center">Frontend dev</div>
+						    <div className="mx-auto mt-3 text-center"><i>Frontend dev</i></div>
 						  </div>
 						</div>
 						<div className="card p-0 col-lg-1 mx-auto border-0" style={{width: "8rem"}}>
@@ -283,64 +283,12 @@ function Home(){
 						    	<a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto me-3" alt="..." style={{width:'30px', height:'3Opx'}}  /></a>
 						    <a href="#"><img src="images/logo.png" className="card-img-top p-0 mx-auto" alt="..." style={{width:'30px', height:'3Opx'}}  /></a><br />
 						    </div>
-						    <div className="mx-auto mt-3 text-center">Frontend dev</div>
+						    <div className="mx-auto mt-3 text-center"><i>Backend dev</i></div>
 						  </div>
 						</div>
 					</div>
 
 				</div>
-
-				<div className="">
-					   <div className=" pt-3 mt-2 text-white" style={{backgroundColor:'#443369'}}>
-				        <div className="container-fluid">
-				            <div
-				                className="row d-flex align-items-center">
-				                <div className="col-lg-4 ps-5">
-				                    <div>
-				                       <a href="#"><img src="images/logo.png" className="card-img-top mx-auto p-0 me-3 rounded-pill" alt="..." style={{width:'100px', height:'100px'}} /> <span className="text-white fs-2 fw-bold">MonAtelier</span></a>
-				                    </div>
-				                    <div className="w-50">
-				                         MonAtelier est une application web de couture dont l’objectif 
-				                         principal est de numériser l’organisation des activités des couturiers
-				                    </div>
-				                </div>
-				                <div className="col-lg-4  text-center">
-				                	<span className="text-white fs-3 fw-bold">Liens rapides</span>
-				                    <div className="active">
-				                    	<a href="" style={{paddingTop: '60px'}}>Accueil</a><br className="mb-3 mt-2"/>
-				                    	<a href="" style={{paddingTop: '60px'}}>Catégories</a><br className="mb-3"/>
-				                    	<a href="" style={{paddingTop: '60px'}}>Modèles</a><br className="mb-3"/>
-				                    	<a href="" style={{paddingTop: '60px'}}>A Propos</a><br className="mb-3"/>
-				                    	<a href="" style={{paddingTop: '60px'}}>Contact</a>
-				                    </div>
-				                </div>
-				                <div className="col-lg-4  text-center">
-				                	<span className="text-white fs-3 fw-bold">Créer un atelier </span><br />
-				                    <a class=" font-weight-normal text-justify " href="#">
-				                    Avec MonAtelier, vous avez la possiblité de gérer votre atelier, entre autre: vos clients, 
-				                    vos commandes et même la possibilité d’améliorer votre visibilité. <br />
-									Enregistrez-vous.</a><br />
-									<div className='pt-2'>
-									<a href="#" style={{color: '#443369', backgroundColor:'white'}}
-				                     className=" font-weight-normal h6 d-inline-block p-2 rounded-pill mb-2 me-2">S'inscire</a>
-				                     <span>OU</span>
-				                     <a href="#" style={{color: 'white'}}
-				                     className=" font-weight-normal h6 d-inline-block p-2 border ms-2 border-2 border-white rounded-pill mb-2 me-2">Se connecter</a>
-
-									</div>
-				                    
-				                </div>
-
-            </div>
-        </div>
-			    </div><hr className="mb-0" style={{backgroundColor: '#c4c4c4'}} />
-			    <div className="d-flex align-items-center text-dark" style={{height: '7vh'}}>
-			        <p className="mx-auto"><span className="">Copyright © 2020 | </span>  
-			        <span>power | </span>
-			        <span className=" h6 my-0">Groupe 4 
-			        </span></p>
-			    </div>
-				</div>	
 			</div>
 
 	
