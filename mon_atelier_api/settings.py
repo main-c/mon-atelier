cd631df87be74d3696f33bd98e911617515d81a4
main-c <yannikkwc@gmail.com>
2022-01-03 12:28:39 +0100

    correct staticfile storage

--- mon_atelier_api/settings.py
+++ mon_atelier_api/settings.py
@@ -42,6 +42,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mon_atelier_api.urls'
