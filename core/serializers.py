391689faf10487f3431a7a10b34c83fbbcad02a4
main-c <yannikkwc@gmail.com>
2022-01-03 05:10:59 +0100

    creating serializer files

---
+++ core/serializers.py
@@ -0,0 +1,4 @@
+from core.models import Worker, Workshop, LocationImage, Mesure, Client, Order, Category, Modele, OrderItem, Article
+from rest_framework import serializers
+
+class WorkshopSerializer(serializers.Serializer)
