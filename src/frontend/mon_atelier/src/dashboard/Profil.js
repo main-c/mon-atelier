b4941c8c112aa5a4a5d6ab4c1d19e73de62af620
Jugalux <jugalux111@gmail.com>
2022-01-12 15:47:36 +0100

    begin of profil page and responsive dashboard

--- src/frontend/mon_atelier/src/dashboard/Profil.js
+++ src/frontend/mon_atelier/src/dashboard/Profil.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import Layout from './Layout.js';
+import './profil.css';
 
 
 function Profil(){
 
 	const profil = (
 
-		<div className="container-fluid">
-			<p className="text-center text-violet">Hey, je suis le composant Profil</p>
+		<div className="profil_container">
+
+			<div className="container-fluid bg_brofil px-0"></div>
+
+			<div className="row justify-content-around">
+				<div className="col-lg-4 p-2 bg-light rounded shadow">
+					<h4>Personal box</h4>
+
+					{/* code des informations personelles du propriétaire de l'atelier */}
+
+				</div>
+				<div className="col-lg-6 p-2 bg-light rounded shadow">
+					<h4>General box</h4>
+
+				{/* code des informations propres à l'atelier */}
+
+				</div>
+			</div>
+
 		</div>
 
 	);
