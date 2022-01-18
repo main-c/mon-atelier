1d63de6e54e0e29a04344ed9b142319450eaf4a2
Rainsong <ngoutsoprainsong@gmail.com>
2022-01-18 12:56:25 +0100

    get articles back

--- src/frontend/mon_atelier/src/components/Article.js
+++ src/frontend/mon_atelier/src/components/Article.js
@@ -1,46 +1,41 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
-
-import './modele.css'
+import {Link, useParams} from 'react-router-dom';
 
 
-class Article extends React.Component {
+export default function Article() {
 
-	state = {
-		articles : []
-	}
+	const [articles,setArticles]= useState([])
 
-	componentDidMount(){
-		axios.get("https://api-mon-atelier.herokuapp.com/api/v1/articles/")
-		.then(response => {
-			const articles = response.data.results;
-			this.setState({articles});
+	const {nameCat} = useParams()
+	
+	useEffect(() => {
+		axios.get(`https://api-mon-atelier.herokuapp.com/api/v1/articles/?workshop__name=&mesure=&modele__name=&modele__category__name=${nameCat}`)
+		.then(res => {
+			console.log(res.data)
+			setArticles(res.data.results)
 		})
+		.catch(err => {
+			console.log(err)
+		})
+	}, [])
 
-		.then((response) => this.setState({articles: response.data.results}))
-		.catch((err) => console.log(err));
-	}
-
-
-render(){
 		return(
 
-
-				<div className="container-fluid px-0 pb-0">
+				<div className="container-fluid px-0 pb-lg-5">
 					<div className='fw-bold text-center mt-5 fs-3'>NOS ARTICLES</div>
 					<div className="mx-auto mb-5" style={{height:'4px', width:'100px', background:'#FF5566'}}></div>
 
-					<div className="row pt-3 mx-auto bg-violet py-3">
+					<div className="row pt-3 mx-auto bg-violet py-5">
 					
-					{ this.state.articles.slice(0, 4).map(article => {
+					{articles.map(article => {
 
 						return(
 							<div className="col-lg-3 card p-0 mt-3 mx-auto border-0" style={{width: "19rem", maxHeight:'26rem'}}>
 							<img src={article.result} className="card-img-top p-0" alt="..." style={{width:'auto', height:'6.4cm'}}  />
 							<div className="card-body ps-2">
 								<h5 className="card-title">
-									<Link to={`/detail_article/${article.id}`}>
+									<Link to={`/articles/${article.id}`}>
 						            	<p className="text-dark fs-4">{article.name}</p>
 						        	</Link>
 								</h5>
@@ -56,17 +51,12 @@ render(){
 
 							);
 					})}
-					<div className="me-5 mt-3" style={{textAlign:'right'}}>
-						<Link to="/articles" className="border-0 rounded-1 bg-white text-violet fs-5 pt-2 mt-lg-1 mt-5 pb-2 ps-3 pe-3 mora" style={{background:'#443369'}}>voir plus</Link>
-					</div>
+						
+
 
 					</div>
-
-				
 				</div>
 
 	);
 }
-}
 
-export default Article;
