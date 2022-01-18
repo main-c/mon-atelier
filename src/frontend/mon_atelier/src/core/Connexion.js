82b1eef0a1caa9fbe5fd1bec2a491e4047579489
raissa237 <floretiomela@gmail.com>
2022-01-18 09:49:35 +0100

    page account

--- src/frontend/mon_atelier/src/core/Connexion.js
+++ src/frontend/mon_atelier/src/core/Connexion.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-
+import axios from 'axios';
 import './connexion.css';
 
 
@@ -11,65 +11,74 @@ class Connexion extends React.Component{
 		super(props)
 		this.state = {
 			user: "",
-			pass: ""
+			pass: "",
+			isPasswordShown: false
 		}
 		this.handleChange = this.handleChange.bind(this) 
 	}
 
-	handleChange(e){
-		const name = e.target.name
+	handleChange(event){
+		const name = event.target.name
 		this.setState({
-			[name]: e.target.value
+			[name]: event.target.value
 		})
 	}
 
-
-	state1 = {
-		icon: 'fa fa-eye',
-		type: 'password'
+	handleSubmit = event => {
+		event.preventDefault();
+	
+		const connex = {
+			user: this.state.user,
+			pass: this.state.pass
+		  };
+	
+		axios.post(`https://api-mon-atelier.herokuapp.com/api/v1/`,{connex}).then(res => {
+			console.log(res);
+			console.log(res.data);
+		})
+		
+		/* axios.delete(`https://jsonplaceholder.typicode.com/users/${this.state.nom}`)
+		.then(res => {
+		console.log(res);
+		console.log(res.data);
+		})  */
 	}
 
-	changeType = () => {
-		const { type } = this.state1
-		if (type === 'password') {
-			this.setState1({
-				icon: 'fa fa-eye-slash',
-				type: 'text'
-			})
-		} else {
-			this.setState1({
-				icon: 'fa fa-eye',
-				type: 'password'
-			})
-		}
+
+	//visibilite
+
+	togglePasswordShownVisibility = () => {
+		const {isPasswordShown} = this.state;
+		this.setState({isPasswordShown: !isPasswordShown});
 	}
 
 
 	render(){
+
+		const{isPasswordShown} = this.state;
+
 		return(
-			<div className="container-fluid connexion_container py-5 d-flex justify-content-center">
+			<div className="container-fluid connexion_container py-5 ps-lg-5">
 				<section className="section">
-					<Link to="/">
-						<div class="d-flex justify-content-center py-3">
-							<img src="logo.png" className="img" alt="logo_MonAtelier"/>
-							<h3 className="text-white mt-4 ms-1">Connexion</h3>
-						</div>
-					</Link>
+					<div class="div1 text-center py-2">
+						<img src="logo.png" className="img" alt="logo_MonAtelier"/>
+						<h1 className="text-white mt-2">Connexion</h1>
+					</div>
 					<div className="div2">
-						<form action="">
+						<form action="" onSubmit={this.handleSubmit}>
 							<div className="user pt-5">
 								<p className="text-dark fs-5 ms-4 mb-0">Email ou Téléphone</p>
-								<input type="text" name="user" id="user" placeholder="..." className="text-violet px-3 pb-2 px-4" onchange={this.handleChange} required/>
+								<input type="text" name="user" id="user" className="text-violet px-3 pb-2 px-4" onchange={this.handleChange} required/>
 								<span className="focus-border container"></span>
 							</div>
 							<div className="pass mt-4">
 								<div id="eye">
 									<div>
 										<p className="text-dark fs-5 ms-4 mb-0">Mot de passe</p>
-										<input spellCheck={false} type={this.state1.type}  name="pass" placeholder="..." id="pass"  className="text-violet px-3 pb-2 px-4" onchange={this.handleChange} required/>
+										<input type={(isPasswordShown) ? "text" : "password"}  name="pass" id="pass" className="text-violet px-3 pb-2 px-4" onchange={this.handleChange} required/>
 									</div>
-									<div>
-										<i className={`${this.state1.icon} icon`} aria-hidden='true' onClick={this.changeType}></i>
+									<div className="text-violet me-4">
+										<i className={`fa ${isPasswordShown ? "fa-eye":"fa-eye-slash"} icon-eye`} aria-hidden='true' onClick={this.togglePasswordShownVisibility}></i>
 									</div>
 								</div>
 								<span className="focus-border"></span>
@@ -78,7 +87,7 @@ class Connexion extends React.Component{
 					</div>
 					<div class="text-center py-2 div3">
 						<button className="fw-bold my-3">connexion</button>
-						<p className="para text-white">Pas encore connecté ? <strong><Link to="/signup">s'inscrire</Link></strong></p>
+						<p className="para">Pas encore connecté ? <strong><Link to="/signup">s'inscrire</Link></strong></p>
 					</div>
 				</section>
 			</div>
--- src/frontend/mon_atelier/src/core/Inscription.js
+++ src/frontend/mon_atelier/src/core/Inscription.js
@@ -1,134 +1,93 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import { Link } from "react-router-dom";
-
 import './inscription.css';
-
+import axios from 'axios';
 
 
 class Inscription extends React.Component{
 
-	constructor(props) {
-		super(props);
-		this.handleSubmit = this.handleSubmit.bind(this);
-		this.handleChange = this.handleChange.bind(this);
-
+	constructor(props){
+		super(props)
 		this.state = {
-		    last_name: "",
-			name: "",
-			password: "",
-			email: "",
-			phone: ""
-		};
-    }
-	
-
-	handleChange = (e) => {
-	    this.setState({
-	    	[e.target.name]: e.target.value 
-	    });
-	};
-
-	handleSubmit = (e) => {
-	    e.preventDefault();
-	    const workshopData = {
-	    	last_name: this.state.last_name,
-	    	name: this.state.name,
-		    email: this.state.email,
-		    password: this.state.password,
-		    phone: this.state.phone
-	    };
-	    axios.post("https://api-mon-atelier.herokuapp.com/api/v1/workshops/", workshopData)
-	    .then((response) => {
-	      console.log(response.status);
-	    });
-	};
-
-/*
-	handleSubmit = event => {
-	    event.preventDefault();
-
-	    const workshop = {
-	      name: this.state.name,
-	      last_name: this.state.last_name,
-	      password: this.state.password,
-	      email: this.state.email,
-	      phone: this.state.phone
-	    };
-
-	    axios({
-	    	method: "post",
-	    	url: "https://api-mon-atelier.herokuapp.com/api/v1/workshops/",
-	    	data: workshop
-	    })
-	    .then(function(response){
-	    	console.log(response);
-	    })
-	    .catch(function(error){
-	    	console.log(error);
-	    });
+			user: "",
+			pass: "",
+			mail: "",
+			num_user: "",
+			isPasswordShown: false
+		}
+		this.handleChange = this.handleChange.bind(this) 
 	}
-*/
 
+	handleChange(event){
+		const name = event.target.name
+		this.setState({
+			[name]: event.target.value
+		})
+	}
+
+	handleSubmit = event => {
+		event.preventDefault();
+	
+		const user = {
+			user_name: this.state.user_name,
+			user_Atel: this.state.user_Atel,
+			user_pass: this.state.user_pass,
+			user_mail: this.state.user_mail,
+			number_user: this.state.number_user
+		  };
+	
+		axios.post(`https://api-mon-atelier.herokuapp.com/api/v1/`,{ user}).then(res => {
+			console.log(res);
+			console.log(res.data);
+		})
+		
+		/* axios.delete(`https://jsonplaceholder.typicode.com/users/${this.state.nom}`)
+		.then(res => {
+		console.log(res);
+		console.log(res.data);
+		})  */
+	}
+	
 	//visibilite
 
-	state1 = {
-		icon: 'fa fa-eye',
-		type: 'password'
+	togglePasswordShownVisibility = () => {
+		const {isPasswordShown} = this.state;
+		this.setState({isPasswordShown: !isPasswordShown});
 	}
-
-	changeType = () => {
-		const { type } = this.state1
-		if (type === 'password') {
-			this.setState1({
-				icon: 'fa fa-eye-slash',
-				type: 'text'
-			})
-		} else {
-			this.setState1({
-				icon: 'fa fa-eye',
-				type: 'password'
-			})
-		}
-	}
-
 
 	render(){
 
-		const { last_name, name, password, email, phone } = this.state;
-
+		const{isPasswordShown} = this.state;
 
 		return(
-			<div className="container-fluid body mt-0 py-1 d-flex justify-content-center">
-				<section className="ms-lg-5 my-4 my-lg-1 sect_ins" >
-					<Link to="/">
-						<div className="d-flex justify-content-center py-3">
-							<img src="logo.png" className="img" alt="logo_MonAtelier"/>
-							<h3 className="text-white mt-5 ms-1">Inscription</h3>
-						</div>
-					</Link>
-					<form method="GET" action="#" id="sign_up_form" onSubmit={this.handleSubmit}>
+			<div className="container-fluid body mt-0 py-3">
+				<section className="ms-lg-5 my-4 sect_ins" >
+					<div className="text-center pt-3">
+						<img src="logo.png" className="img" alt="logo_MonAtelier"/>
+						<p className="text-white fs-3">Inscription</p>
+					</div>
+					<form action="" id="sign_up_form" onSubmit={this.handleSubmit}>
 						<div className="bg-white py-5">
 							<div className="row">
 								<div className="col-lg-6">
 									<div className="user w-100 mt-4">
 										<p className="text-dark fs-5 ms-4 mb-0">Nom complet</p>
-										<input type="text" className="inp_ins text-violet px-3 pb-2 px-4" placeholder="..." name="last_name" id="user" onChange={this.handleChange} required/>
+										<input type="text" className="inp_ins text-violet px-3 pb-2 px-4" name="user_name" id="user" onchange={this.handleChange} required/>
 										<span className="focus-bord larg"></span>
 									</div>
 									<div className="user w-100 mt-4">
 										<p className="text-dark fs-5 ms-4 mb-0">Nom de l'atelier</p>
-										<input type="text" className="inp_ins text-violet px-3 pb-2 px-4" placeholder="..." name="name" id="atel" onChange={this.handleChange} required/>
+										<input type="text" className="inp_ins text-violet px-3 pb-2 px-4" name="user_atel" id="atel" onchange={this.handleChange} required/>
 										<span className="focus-bord larg"></span>
 									</div>
 									<div className="pass w-100 mt-4">
 										<div id="eye">
 											<div>
 												<p className="text-dark fs-5 ms-4 mb-0">Mot de passe</p>
-												<input className='input' spellCheck={false} type={this.state1.type}  placeholder="..." className="inp_ins text-violet px-3 pb-2 px-4" name="password" id="pass" onChange={this.handleChange} required/>
+												<input className='input' type={(isPasswordShown) ? "text" : "password"} className="inp_ins text-violet px-3 pb-2 px-4" name="user_pass" id="pass" onchange={this.handleChange} required/>
 											</div>
 											<div className="text-violet me-4">
-												<i className={`${this.state1.icon} icon`} id="i" aria-hidden='true' onClick={this.changeType}></i>
+												<i className={`fa ${isPasswordShown ? "fa-eye":"fa-eye-slash"} icon`} id="i" aria-hidden='true' onClick={this.togglePasswordShownVisibility}></i>
 											</div>
 										</div>
 										<span className="focus-bord larg"></span>
@@ -137,19 +96,19 @@ class Inscription extends React.Component{
 								<div className="col-lg-6">
 									<div className="w-100 mt-4">
 										<p className="text-dark fs-5 ms-4 mb-0">E-mail</p>
-										<input type="email" className="inp_ins text-violet px-3 pb-2 px-4" placeholder="..." name="email" id="mail" onChange={this.handleChange} required/>
+										<input type="email" className="inp_ins text-violet px-3 pb-2 px-4" name="user_mail" id="mail" onchange={this.handleChange} required/>
 										<span className="focus-bord"></span>
 									</div>
 									<div className="w-100 mt-4">
 										<p className="text-dark fs-5 ms-4 mb-0">Téléphone</p>
-										<input type="tel" className="inp_ins text-violet px-3 pb-2 px-4" placeholder="..." name="phone" id="num_user" onChange={this.handleChange} required/>
+										<input type="tel" className="inp_ins text-violet px-3 pb-2 px-4" name="number_user" id="num_user" onchange={this.handleChange} required/>
 										<span className="focus-bord"></span>
 									</div>
 								</div>
 							</div>
 						</div>
 						<div className="sect3 p-3 text-center">
-							<button type="submit" className="fw-bold mt-3">S'inscrire</button>
+							<button className="fw-bold mt-3">S'inscrire</button>
 							{/* <input type="submit" className="bouton" value="S'inscrire"></input> */}
 							<p className="mt-2 text-center">Déjà inscris ? <strong><Link to="/login">se connecter</Link></strong></p>
 						</div>
