bc3f380275369e2197e50fd548b3bbe70bf9d2a0
Rainsong <ngoutsoprainsong@gmail.com>
2022-01-24 06:35:05 +0100

    is the code of home the problem?? test2

--- src/frontend/mon_atelier/src/core/Home.js
+++ src/frontend/mon_atelier/src/core/Home.js
@@ -1,30 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Slide, {CarouselItem} from '../components/slide'
-import axios from 'axios'
 
-import Navigation from '../components/Navigation';
-import Footer from '../components/Footer';
-import Categories from '../components/categories'
-import ArticleAll from '../components/articleAll'
 import './Home.css'
 
 class Home extends React.Component{
 
-		state = {
-		articles : []
-	}
 
-	componentDidMount(){
-		axios.get("articles/")
-		.then(response => {
-			const articles = response.data.results;
-			this.setState({articles});
-		})
-
-		.then((response) => this.setState({articles: response.data.results}))
-		.catch((err) => console.log(err));
-	}
 
 	render(){
 		return(
@@ -32,10 +12,7 @@ class Home extends React.Component{
 			<div className="Integration">
 
 			<h1>Home page</h1>
-				<Navigation />
 			
-
-				<Footer />
 			</div>
 
 		);
