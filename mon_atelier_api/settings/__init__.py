a76cf0d91e089600f213e6e2508e1303c4e0106c
Yannick Kadjie <kadjie_main_cbp@_HOSTNAME_>
2022-01-30 12:44:44 +0100

    fix : trying to fix output and input error

--- mon_atelier_api/settings/__init__.py
+++ mon_atelier_api/settings/__init__.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = 'mon_atelier_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'monatelier',
+        'USER': 'main_c',
+        'PASSWORD': 'yannikkadjie',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
